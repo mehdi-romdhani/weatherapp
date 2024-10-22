3b164596dd1b341d313eddda5dd02da17c57ff4d
Mehdi Romdhani <mehdiromdhani@MacBook-Air-de-Mehdi.local>
2024-10-22 14:24:34 +0200

    add APIweather

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,15 +1,16 @@
 import { StyleSheet } from 'react-native';
-
+import React, { useCallback, useState } from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import WeatherScreen from '@/components/WeatherScreen';
+import { useFocusEffect } from 'expo-router';
 
 export default function TabOneScreen() {
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text>Hello World</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <WeatherScreen />
     </View>
   );
 }
